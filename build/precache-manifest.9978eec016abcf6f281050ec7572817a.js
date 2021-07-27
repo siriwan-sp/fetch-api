6a43f7ccbbd39b0446c784173503fe6b69fe4882
siriwan.sp <siriwan.sp36@gmail.com>
2021-07-27 12:41:04 +0700

    first commit

---
+++ build/precache-manifest.9978eec016abcf6f281050ec7572817a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b827c32d6da30a4f3ca558b79b8c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188accfebcc0cfc97b90",
+    "url": "/static/css/main.c38bb64e.chunk.css"
+  },
+  {
+    "revision": "905d85f75bd7fc66e6e1",
+    "url": "/static/js/2.96e1f9b1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.96e1f9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188accfebcc0cfc97b90",
+    "url": "/static/js/main.ea82e423.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Movie.module.css';
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+      <h1>{props.producer}</h1>
+    </li>
+  );
+};
+
+export default Movie;
